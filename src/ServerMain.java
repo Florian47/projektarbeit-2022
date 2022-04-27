093e03eb40535879a68d88e2289e29733283e082
Florian <weinertflorian@googlemail.com>
2022-04-28 01:28:36 +0200

    fixed missing librarys and added database support for login and register

--- src/ServerMain.java
+++ src/ServerMain.java
@@ -1,18 +1,29 @@
 package src;
 
+import org.apache.derby.iapi.jdbc.AutoloadedDriver;
 import org.glassfish.grizzly.http.server.HttpServer;
 import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
 import org.glassfish.jersey.server.ResourceConfig;
 
 import java.io.IOException;
 import java.net.URI;
+import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class ServerMain {
-    public static void main( String[] args ) throws IOException, InterruptedException
-    {
+    public static void main( String[] args ) throws IOException, InterruptedException, SQLException {
+        //cache db connection on startup
+        DBConnection.get().setAutoCommit(true);
+        DBConnection.dropDatabaseContent();
+        DBConnection.fillWithTestData();
+
+
+
+
         String baseUrl = ( args.length > 0 ) ? args[0] : "http://localhost:4000";
         ResourceConfig config = new ResourceConfig( UserResource.class );
-        config.register(new FilterCors());
         config.register(FilterCors.class);
 
         final HttpServer server = GrizzlyHttpServerFactory.createHttpServer(
@@ -23,8 +34,10 @@ public class ServerMain {
                 server.shutdownNow();
             }
         } ) );
+
         server.start();
         Thread.currentThread().join();
+
     }
 
 }
--- src/UserResource.java
+++ src/UserResource.java
@@ -5,6 +5,8 @@ import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.UriInfo;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 @Path("users")
 @Produces(MediaType.APPLICATION_JSON)
@@ -13,30 +15,85 @@ public class UserResource {
 
     @POST
     @Path("/authenticate")
-    public Response login(User login) {
+    public Response login(User user) throws SQLException {
+        User dbUser = getUserByUserNameAndPassword(user);
+        if(dbUser != null) {
+            return Response.ok(dbUser).build();
+        }
+        return Response.serverError().build();
+    }
 
-        return Response.ok(login).build();
+    @POST
+    @Path("/register")
+    public Response register(User user) throws SQLException {
+        addUser(user);
+        User dbUser = getUserByUserNameAndPassword(user);
+        return login(dbUser);
     }
 
     @GET
     @Path("/{id}")
-    public Response getUserById(@PathParam("id") Long id,
-                                @QueryParam("benutzername") @DefaultValue("") String title) {
+    public Response getUserById(@PathParam("id") Long id) {
         // ...
         return null;
     }
 
     @POST
-    public Response getUserById(User userToStore, @Context UriInfo uriInfo) {
+    public Response getUser(User userToStore) {
         // ...
         return null;
     }
 
     @DELETE
     @Path("/{id}")
-    public Response deleteUser(@PathParam("id") Long id,
-                               @HeaderParam("User-Agent") String userAgent) {
+    public Response deleteUser(@PathParam("id") Long id) {
         // ...
+        return null;
+    }
+
+    public void addUser(User user) throws SQLException {
+        try (var con = DBConnection.get().createStatement()) {
+            String sql = "INSERT INTO SCHUELER (firstname, lastname, username, password) VALUES ('%s', '%s', '%s', '%s')".formatted(user.getVorname(), user.getNachname(), user.getBenutzername(), user.getPasswort());
+            con.execute(sql);
+        }
+    }
+
+    public void updateUser(User user) throws SQLException {
+        try (var con = DBConnection.get().createStatement()) {
+            String sql = "UPDATE schueler SET firstname = '%s', lastname = '%s', username = '%s', password = '%s' WHERE id = %s".formatted(user.getVorname(), user.getNachname(), user.getBenutzername(), user.getPasswort(), user.getId());
+            con.execute(sql);
+        }
+    }
+
+    public void deleteUser(User user) throws SQLException {
+        try (var con = DBConnection.get().createStatement()) {
+            con.execute("DELETE FROM schueler WHERE id=%s".formatted(user.getId()));
+        }
+    }
+
+    public User getUserByUserNameAndPassword(User user) throws SQLException {
+        try (var con = DBConnection.get().createStatement()) {
+            ResultSet rs = con.executeQuery("SELECT * FROM schueler WHERE(username='%s' AND password='%s')".formatted(user.getBenutzername(), user.getPasswort()));
+            return map(rs);
+        }
+    }
+    public User getUserById(User user) throws SQLException {
+        try (var con = DBConnection.get().createStatement()) {
+            ResultSet rs = con.executeQuery("SELECT * FROM schueler WHERE(id=%s)".formatted(user.getId()));
+            return map(rs);
+        }
+    }
+    
+    private User map(ResultSet resourceResult) throws SQLException {
+        if (resourceResult.next()) {
+            User user = new User();
+            user.setVorname(resourceResult.getString("firstname"));
+            user.setNachname(resourceResult.getString("lastname"));
+            user.setBenutzername(resourceResult.getString("username"));
+            user.setPasswort(resourceResult.getString("password"));
+            user.setId(resourceResult.getInt("id"));
+            return user;
+        }
         return null;
     }
 
