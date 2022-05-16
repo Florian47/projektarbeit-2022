7249aa129f0922d08d7912acd4a2d26646907255
chris <chris.brinkhofg@gmail.com>
2022-05-17 01:41:07 +0200

    task repo update

--- src/main/java/sommersemester2022/studentGeneratedTraining/StudentGeneratedTrainingController.java
+++ src/main/java/sommersemester2022/studentGeneratedTraining/StudentGeneratedTrainingController.java
@@ -1,41 +1,45 @@
 package sommersemester2022.studentGeneratedTraining;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import sommersemester2022.task.TaskController;
 import sommersemester2022.task.TaskEntity;
+import sommersemester2022.task.TaskRepo;
 import sommersemester2022.training.TrainingController;
 import sommersemester2022.training.TrainingEntity;
+import sommersemester2022.training.TrainingRepo;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 public class StudentGeneratedTrainingController {
-
-  private TaskController taskController = new TaskController();
-  private TrainingController trainingController = new TrainingController();
+  @Autowired
+  private TaskRepo taskRepo;
+  @Autowired
+  private TrainingRepo trainingRepo;
 
   @PostMapping("/studentGeneratedTraining/add")
-  public void createStudentGeneratedTraining(@RequestBody StudentGeneratedTrainingEntity generatedTraining) {
-    List<TaskEntity> allTasks = taskController.getAllTasksForGeneratedTraining(generatedTraining.getTrainingCategory(), generatedTraining.getTrainingDifficulty());
+  public TrainingEntity createStudentGeneratedTraining(@RequestBody StudentGeneratedTrainingEntity generatedTraining) {
+    List<TaskEntity> allTasks = taskRepo.findAllByCategoryAndDifficulty( generatedTraining.getTrainingCategory(), generatedTraining.getTrainingDifficulty() );
     List<Integer> indices = new ArrayList<>();
 
     // Create as many random numbers as asked from student and put into list
-    for (int i = 0; i <= generatedTraining.getTaskAmount(); i++) {
+    for (int i = 0; i < generatedTraining.getTaskAmount(); i++) {
       int index;
       do {
-        index = (int)(Math.random() * taskController.getAll().size());
+        index = (int) (Math.random() * taskRepo.count());
       } while (indices.contains(index));
-      allTasks.get(index);
+      indices.add(index);
     }
     // Create the official training with given amount of tasks
     TrainingEntity training = new TrainingEntity("", null, false);
-    for (int i = 0; i <= indices.size(); i++) {
+    for (int i = 0; i < indices.size(); i++) {
       training.addTask(allTasks.get(indices.get(i)));
     }
-    trainingController.createTraining(training);
+    return trainingRepo.save(training);
     // Token zur Identifizierung, Creator muss hinzugefügt werden
   }
 
