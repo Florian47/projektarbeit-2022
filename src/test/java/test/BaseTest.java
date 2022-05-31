24c089587b67c17a6bc4ba219c5d935050127218
Florian <weinertflorian@googlemail.com>
2022-06-01 00:21:33 +0200

    add jwt tokens to tests

--- src/test/java/test/BaseTest.java
+++ src/test/java/test/BaseTest.java
@@ -19,6 +19,7 @@ import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.TransactionStatus;
 import org.springframework.transaction.support.TransactionCallbackWithoutResult;
 import org.springframework.transaction.support.TransactionTemplate;
+import sommersemester2022.security.services.jwt.JwtUtils;
 
 import javax.persistence.EntityManager;
 import java.util.List;
@@ -38,6 +39,8 @@ public class BaseTest {
   protected PlatformTransactionManager tx;
   @Autowired
   protected EntityManager em;
+  @Autowired
+  protected JwtUtils jwtUtils;
 
   @BeforeEach
   public void setup() {
@@ -48,9 +51,21 @@ public class BaseTest {
     });
   }
 
+
+  protected String getJWTToken(String user) {
+    return jwtUtils.generateJwtToken(user);
+  }
+
   protected ResponseEntity<String> restPost(String url, String json) {
     HttpHeaders headers = new HttpHeaders();
     headers.setContentType(MediaType.APPLICATION_JSON);
+    return restTemplate.postForEntity(url, new HttpEntity<>(json, headers), String.class);
+  }
+
+  protected ResponseEntity<String> restAuthPost(String url, String json, String token) {
+    HttpHeaders headers = new HttpHeaders();
+    headers.setContentType(MediaType.APPLICATION_JSON);
+    headers.setBearerAuth(token);
     return restTemplate.postForEntity(url, new HttpEntity<>(json, headers), String.class);
   }
 
