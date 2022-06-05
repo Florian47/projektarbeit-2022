cec1d39de8695d8ed0bb915fd1870963c9579d88
Florian <florian.weinert.42@googlemail.com>
2022-06-05 14:10:35 +0200

    prevent fail of generating json data because a field is empty / not accessable

--- src/main/java/sommersemester2022/config/JacksonConfiguration.java
+++ src/main/java/sommersemester2022/config/JacksonConfiguration.java
@@ -2,15 +2,24 @@ package sommersemester2022.config;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 
 @Configuration
 public class JacksonConfiguration {
+
+  @Bean
+  public MappingJackson2HttpMessageConverter mappingJackson2HttpMessageConverter() {
+    ObjectMapper mapper = new ObjectMapper();
+    mapper.configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false);
+    return new MappingJackson2HttpMessageConverter(mapper);
+  }
 
   @Bean
   @Primary
